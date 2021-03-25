acca810d74a5df31e46d32a4178e8309038f693c
Amy Zhu <ajyzhu@gmail.com>
2021-03-25 10:33:35 -0700

    Finish up visualization (mostly)

--- ablation/visualize.py
+++ ablation/visualize.py
@@ -4,13 +4,16 @@ import pandas as pd
 from functools import reduce
 from itertools import groupby
 import json
+from colorsys import hsv_to_rgb
+from random import randint, uniform
+
 
 data = json.load(open("parsed.json"))
 bool_data = list(filter(lambda x: x['domain'] == "bool", data))
 bv4ns_data = list(filter(lambda x: x['domain'] == "bv4ns", data))
 # print(bool_data)
-def make_choose_eqs_time_plot(data):
-    make_choose_eqs_plot(data, lambda x: x["real"])
+def make_choose_eqs_time_rules_plot(data):
+    make_choose_eqs_plot(data, lambda x: x["learned"]["time"], lambda x: x["learned"]["rules"])
 
 def make_choose_eqs_plot(data, compare, compare2):
     # todo there must be some way to deal with the iterable 
@@ -32,7 +35,11 @@ def make_choose_eqs_plot(data, compare, compare2):
 
     # add a minimize bar
     mrat.bar(x, y, width, color='blue')
+    mrat.set(xlabel="choose_eqs setting", ylabel="s")
+    mrat.set_title("Time")
     minimize.bar(x, y2, width, color='red')
+    minimize.set(xlabel="choose_eqs setting", ylabel="Rules Learned")
+    minimize.set_title("Rules Learned")
 
     # minimize.plot()
     plt.show()
@@ -46,13 +53,14 @@ def make_choose_eqs_line_plot(data):
     mrat = list(filter(lambda x: x['type'] == 'mrat', data))
     minimize = list(filter(lambda x: x['type'] == 'minimize', data))
 
+    
     # want to find all the runs for different types
     orat_egraphs = aggregate_egraphs_list(list(map(lambda x: x["egraphs"], orat)))
     minimize_egraphs = aggregate_egraphs_list(list(map(lambda x: x["egraphs"], minimize)))
     mrat.sort(key=lambda x: x["mrat_m"])
     mrat_egraphs_by_m = [list(v) for k,v in groupby(mrat, lambda x: x["mrat_m"])]
     mrat_egraphs_by_m = [dict([('m', lst[0]["mrat_m"]), \
-        ('e', aggregate_egraphs_list(list(map(lambda x: x["egraphs"], lst))))]) for lst in mrat_egraphs_by_m]
+        ('egraph', aggregate_egraphs_list(list(map(lambda x: x["egraphs"], lst))))]) for lst in mrat_egraphs_by_m]
     print(mrat_egraphs_by_m)
     # so now we have aggregated each iteration over all runs
     # we plot the iterations
@@ -63,22 +71,39 @@ def make_choose_eqs_line_plot(data):
     # plt.scatter(dates,values)
     # plt.plot(dates, values)
 
-    total = max(len(orat_egraphs), len(minimize_egraphs), max([len(x) for x in mrat_egraphs_by_m]))
-    x = list(range(1, total))
+    # total = max(len(orat_egraphs), len(minimize_egraphs), max([len(x) for x in mrat_egraphs_by_m]))
+    # x = list(range(1, total))
 
-    print(total)
-    
-    plt.scatter(list(range(1, len(orat_egraphs))), orat_egraphs, color="r")
-    plt.plot(list(range(1, len(orat_egraphs))), orat_egraphs, color="r")
-    
-    plt.scatter(list(range(1, len(minimize_egraphs))), minimize_egraphs, color="b")
-    plt.plot(list(range(1, len(minimize_egraphs))), minimize_egraphs, color="b")
+    # print(total)
 
-    for m in range(max([len(x) for x in mrat_egraphs_by_m])):
-        plt.scatter(list(range(1, len(m))), m, color="y")
-        plt.plot(list(range(1, len(m))), m, color="y")
+    selector = lambda x: x['e']
+    
+    plt.scatter(list(range(0, len(orat_egraphs))), [selector(x) for x in orat_egraphs], color="r")
+    plt.plot(list(range(0, len(orat_egraphs))), [selector(x) for x in orat_egraphs], color="r", label="orat")
+    
+    plt.scatter(list(range(0, len(minimize_egraphs))), [selector(x) for x in minimize_egraphs], color="b")
+    plt.plot(list(range(0, len(minimize_egraphs))), [selector(x) for x in minimize_egraphs], color="b", label="minimize")
+
+
+    h = 0.27 # green hue in HSV space
+    increment = 0.7 / len(mrat_egraphs_by_m) 
+    print(increment)
+
+    for (i, m) in enumerate(mrat_egraphs_by_m): #range(max([len(x) for x in mrat_egraphs_by_m])):
+        print(m)
+        data = m["egraph"]
+        
+        s = 0.2 + increment * i
+        v = 0.3 + increment * i
+        r, g, b = hsv_to_rgb(h, s, v)
+        # Convert to 0-1 range for HTML output
+        # r, g, b = [x*255 for x in (r, g, b)]
+
+        plt.scatter(list(range(0, len(data))), [selector(x) for x in data], color=(r, g, b, 1))
+        plt.plot(list(range(0, len(data))), [selector(x) for x in data], color=(r, g, b, 1), label="mrat m=" + m["m"])
 
     # minimize.plot()
+    plt.legend()
     plt.show()
 
 def aggregate_egraphs_list(our_list): 
@@ -98,9 +123,7 @@ def aggregate_egraphs_list(our_list):
             ('n', float(x["n"]) + acc["n"])]), \
             all,
             {'e': 0.0, 'cv': 0.0, 'n': 0.0})
-        print(one)
         total.append(one)
-    print(total)
 
 
     return total
@@ -141,17 +164,58 @@ def make_phase_time_plot(data):
     plt.show()
     # sum over all 
 
-# make_choose_eqs_time_plot(bool_data)
-# make_choose_eqs_time_plot(bv4ns_data)
+def compare_run_rewrites(data):
+
+    y1 = lambda x: float(x["learned"]["time"])
+    y2 = lambda x: float(x["learned"]["rules"])
+    # TODO: e or n?
+    y3 = lambda x: sum([int(d["e"]) for d in x["egraphs"]]) / len(x["egraphs"])
+
+    run_rewrites = list(filter(lambda x: x['type'] == 'no-run-rewrites', data))
+    no_run_rewrites = list(filter(lambda x: x['type'] == 'minimize', data)) # regular data
+
+    # rr_x = list(map(lambda d: x(d) , run_rewrites))
+    x = ["No RR", "RR"]
+    rr_y1 = list(map(lambda d: y1(d) , run_rewrites))
+    rr_y1 = sum(rr_y1) / len(rr_y1)
+    rr_y2 = list(map(lambda d: y2(d) , run_rewrites))
+    rr_y2 = sum(rr_y2) / len(rr_y2)
+    rr_y3 = list(map(lambda d: y3(d) , run_rewrites))
+    print(rr_y3)
+    rr_y3 = sum(rr_y3) / len(rr_y3)
+    
+    no_rr_y1 = list(map(lambda d: y1(d) , no_run_rewrites))
+    no_rr_y1 = sum(no_rr_y1) / len(no_rr_y1)
+    no_rr_y2 = list(map(lambda d: y2(d) , no_run_rewrites))
+    no_rr_y2 = sum(no_rr_y2) / len(no_rr_y2)
+    no_rr_y3 = list(map(lambda d: y3(d) , no_run_rewrites))
+    print(no_rr_y3)
+    no_rr_y3 = sum(no_rr_y3) / len(no_rr_y3)
+
+    fig, (time, rules, egraphs) = plt.subplots(1,3)
+
+    width = 0.4
+
+    # average them together
+
+    # add a minimize bar
+    time.bar(x, [rr_y1, no_rr_y1], width, color='thistle')
+    # time.set(xlabel="X", ylabel="Y")
+    time.set_title("Time (s)")
+    rules.bar(x, [rr_y2, no_rr_y2], width, color='cadetblue')
+    # rules.set(xlabel="X", ylabel="Y")
+    rules.set_title("Rules")
+    egraphs.bar(x, [rr_y3, no_rr_y3], width, color='gold')
+    # egraphs.set(xlabel="X", ylabel="Y")
+    egraphs.set_title("Egraph size (eclasses)")
+
+
+    # minimize.plot()
+    plt.show()
+
+# make_choose_eqs_time_rules_plot(bool_data)
+# make_choose_eqs_time_rules_plot(bv4ns_data)
 # make_phase_time_plot(bool_data)
-make_choose_eqs_plot(bool_data, lambda x: x["learned"]["time"], lambda x: x["learned"]["rules"])
-make_choose_eqs_plot(bv4ns_data, lambda x: x["learned"]["time"], lambda x: x["learned"]["rules"])
+compare_run_rewrites(bool_data)
+
 # make_choose_eqs_line_plot(bool_data)
-
-# filter by type and plot accordingly
-
-# x = np.array([5,7,8,7,2,17,2,9,4,11,12,9,6])
-# y = np.array([99,86,87,88,111,86,103,87,94,78,77,85,86])
-
-# plt.scatter(x, y)
-# plt.show()
