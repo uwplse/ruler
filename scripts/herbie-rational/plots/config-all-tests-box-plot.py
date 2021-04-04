efbb2f67c014a37e81ebc8b0ebbc3b3f824bbec9
Chandrakana Nandi <cnandi@cs.washington.edu>
2021-04-03 21:42:29 -0700

    boxplot reordering, works this time

--- scripts/herbie-rational/plots/config-all-tests-box-plot.py
+++ scripts/herbie-rational/plots/config-all-tests-box-plot.py
@@ -9,17 +9,24 @@ field = sys.argv[2]
 with open(jsonp, 'r') as f:
     ss = json.load(f)
 
-labs = [s['config'] for s in ss]
 data = [s['data'] for s in ss]
+pairs = dict([(s['config'], s['data']) for s in ss])
+
+hno = ('herbie-no-simpl', pairs['herbie-no-simpl'])
+ho = ('herbie-only', pairs['herbie-only'])
+ro = ('ruler-only', pairs['ruler-only'])
+hr = ('herbie-ruler', pairs['herbie-ruler'])
+
+new_pairs = dict([hno, ho, ro, hr])
 
 fig, ax = plt.subplots()
-ax.boxplot(data)
+ax.boxplot(new_pairs.values())
 
 title = "config vs " + str(field) + " over seeds, summed over all tests"
 ax.set_title(title)
 ax.set_xlabel('config')
 ax.set_ylabel(str(field))
-ax.set_xticklabels(labs)
+ax.set_xticklabels(new_pairs.keys())
 
 plt.tight_layout()
 plt.savefig('by-config-all-tests-{}-boxplot.pdf'.format(field))
