d44cf6c5f8c481b0c19b26a28adf8e5f1324682e
Chandrakana Nandi <cnandi@cs.washington.edu>
2021-04-03 20:06:07 -0700

    make node limit a CL arg

--- scripts/herbie-rational/herbie-eval.sh
+++ scripts/herbie-rational/herbie-eval.sh
@@ -10,18 +10,32 @@ while [ -L "$src" ]; do
 done
 MYDIR="$(cd -P "$(dirname "$src")" && pwd)"
 
+NSEEDS=${1:-1}
+NUM_NODES=${2:-5000}
+
+if [ "$#" -eq 0 ]; then
+    echo "Running $0 with $NSEEDS seeds and $NUM_NODES enodes."
+else
+    if [ "$#" -eq 1 ]
+    then
+        NSEEDS="$1"
+        echo "Running $0 with $NSEEDS seeds and $NUM_NODES enodes."
+    elif [ "$#" -eq 2 ]
+    then
+        NEEDS="$1"
+        NUM_NODES="$2"
+        echo "Running $0 with $NSEEDS seeds and $NUM_NODES enodes."
+    else
+        echo "Illegal number of arguments."
+        echo "Usage: $0 NSEEDS NUM_NODES."
+        exit 1
+    fi
+fi
+
 tstamp="$(date "+%Y-%m-%d_%H%M")"
 DIR="$MYDIR/output/ruler-herbie-eval"
 
 mkdir -p "$DIR"
-
-# determine number of seeds to sample
-if [ -z "$1" ]; then
-  echo "Usage: $0 NUM_SEEDS"
-  exit 1
-else
-  NSEEDS="$1"
-fi
 
 echo "Running Ruler"
 # run ruler and put rules in output directory
@@ -37,6 +51,8 @@ then
 else
     git clone git@github.com:uwplse/herbie.git
     cd herbie
+    # TODO: remove this once merged.
+    git checkout egg-interface-fixes
     make install
     cd ..
 fi
@@ -66,7 +82,7 @@ export HERBIE="$DIR/herbie"
 popd
 
 # all configs
-configs=("herbie-only" "herbie-ruler" "herbie-no-simpl" "ruler-only")
+configs=("herbie-no-simpl" "herbie-only" "ruler-only" "herbie-ruler")
 
 # for all the configs
 for c in ${configs[@]}; do
@@ -75,7 +91,7 @@ for c in ${configs[@]}; do
     python3 preprocess.py $DIR/ruler-rules-"$tstamp".txt "$HERBIE"/src/syntax/rules.rkt $c
     # do the herbie runs with this config
     # seed-variance.sh will put its output in the right directory
-    ./seed-variance.sh "$NSEEDS" $c $tstamp
+    ./seed-variance.sh "$NSEEDS" "$NUM_NODES" $c $tstamp
 done
 
 # make unified all_all.json with all.json from all configs
