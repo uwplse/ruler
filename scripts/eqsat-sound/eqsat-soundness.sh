ac704d20ecd2dae62c3c84e1053ff35cf4b706a6
Chandrakana Nandi <cnandi@cs.washington.edu>
2021-04-09 14:23:41 -0700

    update scipts

--- scripts/eqsat-sound/eqsat-soundness.sh
+++ scripts/eqsat-sound/eqsat-soundness.sh
@@ -14,23 +14,29 @@ DIR="$MYDIR/output/$tstamp"
 mkdir -p "$DIR"
 
 #TODO: also do 16 bit bv?
-is=2
-vs=3
+is=1
+vs=1
 # domain=("4" "32" "rational")
-domain=("4" "32")
-numfuzz=("0" "10" "100" "1000" "10000")
-cvec=("cvec-none" "only-consts" "only-rand" "cross-prod" "rand-and-consts")
+# domain=("4" "32")
+# numfuzz=("0" "10" "100" "1000" "10000")
+# consts=("1" "2" "3" "4" "5")
+domain=("4")
+numfuzz=("10" "100")
+consts=("1" "2")
+
+default_num_const=5
+samples=10
 
 for d in ${domain[@]}; do
-    for c in ${cvec[@]}; do
+    for c in ${consts[@]}; do
        for n in ${numfuzz[@]}; do
-           resn="$DIR/$d-$c-$n"
+           resn="$DIR/$d-const-$c-fuzz-$n"
            mkdir -p "$resn"
            pushd "$resn"
            if [ "$d" -eq "4" ] || [ "$d" -eq "32" ]; then
-               cargo run --bin bvz3_"$d" --release -- synth --iters "$is" --variables "$vs" --"$c" --no-conditionals --num-fuzz $n
+               cargo run --bin bv"$d" --release -- synth --iters "$is" --variables "$vs" --important-cvec-offsets "$c" --no-conditionals --num-fuzz "$n"
            else
-               cargo run --bin "$d" --release -- synth --iters "$is" --variables "$vs" --"$c" --no-conditionals --num-fuzz $n
+               cargo run --bin "$d" --release -- synth --iters "$is" --variables "$vs" --important-cvec-offsets "$c" --no-conditionals --num-fuzz "$n"
            fi
            post=$("$MYDIR"/postpass.sh out.json $d)
            cat out.json | jq --argjson POST "$post" --argjson DOM "$d" '. + {"domain": $DOM} + {"post_pass": $POST}' > tmp.json
@@ -38,13 +44,13 @@ for d in ${domain[@]}; do
            popd
        done
        # smt validation for each domain and cvec config
-       res="$DIR/$d-$c"
+       res="$DIR/$d-const-$c"
        mkdir -p "$res"
        pushd "$res"
        if [ "$d" -eq "4" ] || [ "$d" -eq "32" ]; then
-           cargo run --bin bvz3_"$d" --release -- synth --iters "$is" --variables "$vs" --"$c" --no-conditionals --use-smt
+           cargo run --bin bv"$d" --release -- synth --iters "$is" --variables "$vs" --important-cvec-offsets "$c" --no-conditionals --use-smt
        else
-           cargo run --bin "$d" --release -- synth --iters "$is" --variables "$vs" --"$c" --no-conditionals --use-smt
+           cargo run --bin "$d" --release -- synth --iters "$is" --variables "$vs" --important-cvec-offsets "$c" --no-conditionals --use-smt
        fi
        # this should always be 0 for sure since the rules are already smt validated.
        post=$("$MYDIR"/postpass.sh out.json $d)
@@ -53,15 +59,15 @@ for d in ${domain[@]}; do
        popd
     done
 
-    # all fuzz configs with default cvec config, i.e., rand_and_cross
+    # all fuzz configs with cvecs made by cross prod of default_num_const important numbers and samples
     for n in ${numfuzz[@]}; do
-        def="$DIR/$d/rand-cross/$n"
+        def="$DIR/$d/sample-$samples-const-$default_num_const/$n"
         mkdir -p "$def"
         pushd "$def"
         if [ "$d" -eq "4" ] || [ "$d" -eq "32" ]; then
-            cargo run --bin bvz3_"$d" --release -- synth --iters "$is" --variables "$vs" --no-conditionals --num-fuzz $n
+            cargo run --bin bv"$d" --release -- synth --iters "$is" --variables "$vs" --no-conditionals --num-fuzz "$n" --important-cvec-offsets "$default_num_const" --n-samples "$samples"
         else
-            cargo run --bin "$d" --release -- synth --iters "$is" --variables "$vs" --no-conditionals --num-fuzz $n
+            cargo run --bin "$d" --release -- synth --iters "$is" --variables "$vs" --no-conditionals --num-fuzz "$n" --important-cvec-offsets "$default_num_const" --n-samples "$samples"
         fi
         post=$("$MYDIR"/postpass.sh out.json $d)
         cat out.json | jq --argjson POST "$post" --argjson DOM "$d" '. + {"domain": $DOM} + {"post_pass": $POST}' > tmp.json
@@ -69,14 +75,14 @@ for d in ${domain[@]}; do
         popd
     done
 
-    # smt validation with default cvec config, i.e., rand_and_cross
-    defs="$DIR/$d/rand-cross/smt"
+    # smt validation with cvecs made by cross prod of default_num_const important numbers and samples
+    defs="$DIR/$d/sample-$samples-const-$default_num_const/smt"
     mkdir -p "$defs"
     pushd "$defs"
     if [ "$d" -eq "4" ] || [ "$d" -eq "32" ]; then
-        cargo run --bin bvz3_"$d" --release -- synth --iters "$is" --variables "$vs" --no-conditionals --use-smt
+        cargo run --bin bv"$d" --release -- synth --iters "$is" --variables "$vs" --no-conditionals --use-smt --important-cvec-offsets "$default_num_const" --n-samples "$samples"
     else
-        cargo run --bin "$d" --release -- synth --iters "$is" --variables "$vs" --no-conditionals --use-smt
+        cargo run --bin "$d" --release -- synth --iters "$is" --variables "$vs" --no-conditionals --use-smt --important-cvec-offsets "$default_num_const" --n-samples "$samples"
     fi
     post=$("$MYDIR"/postpass.sh out.json $d)
     cat out.json | jq --argjson POST "$post" --argjson DOM "$d" '. + {"domain": $DOM} + {"post_pass": $POST}' > tmp.json
@@ -84,26 +90,29 @@ for d in ${domain[@]}; do
     popd
 done
 
-pushd "$DIR"
-# process all the jsons and populate a top-level all.json file
-echo "[ " > all.json
-first=true
+# pushd "$DIR"
+# # process all the jsons and populate a top-level all.json file
+# echo "[ " > all.json
+# first=true
 
-for out in $(find . -name 'out.json' | sort); do
-    if $first; then
-      first=false
-    else
-      echo "," >> all.json
-    fi
-    # for Ruler crashes
-    if [ -s "$out" ]; then
-        jq '{"status": CRASHED, "domain" : .domain, "time": 0.0, "num_rules": 0.0, "post_unsound": 0, "params": {} }' >> all.json 
-    else 
-        cat "$out" \
-            | jq '{status: SUCCESS, "domain" : .domain, "time": .time, "num_rules": .num_rules, "post_unsound": .post_pass, "params": .params}' \
-            >> all.json
-    fi
-done
+# crash="crash"
+# success="success"
 
-echo "]" >> all.json
-popd
+# for out in $(find . -name 'out.json' | sort); do
+#     if $first; then
+#       first=false
+#     else
+#       echo "," >> all.json
+#     fi
+#     # for Ruler crashes
+#     if [ -s "$out" ]; then
+#         jq --arg CRASH "$crash" '{"status": $CRASH, "domain" : .domain, "time": 0.0, "num_rules": 0, "post_unsound": 0, "params": null }' >> all.json 
+#     else 
+#         cat "$out" \
+#             | jq --arg SUCCESS "$success" '{"status": $SUCCESS, "domain" : .domain, "time": .time, "num_rules": .num_rules, "post_unsound": .post_pass, "params": .params}' \
+#             >> all.json
+#     fi
+# done
+
+# echo "]" >> all.json
+# popd
