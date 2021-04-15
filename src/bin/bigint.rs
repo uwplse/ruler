ab23538ce8cbe0121eb2a72579a42484a6b13a61
Zach Tatlock <ztatlock@cs.washington.edu>
2021-04-15 07:32:29 -0700

    looking at bigint

--- src/bin/bigint.rs
+++ src/bin/bigint.rs
@@ -2,13 +2,11 @@ use egg::*;
 use ruler::*;
 
 use num::bigint::{BigInt, RandBigInt, ToBigInt};
-use num::{rational::Ratio, Signed, ToPrimitive, Zero};
-use rand::Rng;
 use rand_pcg::Pcg64;
 use z3::ast::Ast;
 use z3::*;
 
-pub type Constant = Ratio<BigInt>;
+pub type Constant = BigInt;
 
 define_language! {
     pub enum Math {
@@ -17,19 +15,8 @@ define_language! {
         "*" = Mul([Id; 2]),
         "/" = Div([Id; 2]),
         "~" = Neg(Id),
-        "fabs" = Abs(Id),
-        "pow" = Pow([Id; 2]),
-        "recip" = Reciprocal(Id),
         Num(Constant),
         Var(egg::Symbol),
-    }
-}
-
-fn mk_constant(n: &BigInt, d: &BigInt) -> Option<Constant> {
-    if d.is_zero() {
-        None
-    } else {
-        Some(Ratio::new(n.clone(), d.clone()))
     }
 }
 
@@ -48,31 +35,10 @@ impl SynthLanguage for Math {
             Math::Num(n) => vec![Some(n.clone()); cvec_len],
             Math::Var(_) => vec![],
             Math::Div([a, b]) => map!(v, a, b => {
-                if b.is_zero() {
+                if *b == 0.to_bigint().unwrap() {
                     None
                 } else{
                     Some(a / b)
-                }
-            }),
-            Math::Abs(a) => map!(v, a => Some(a.abs())),
-            Math::Pow([a, b]) => map!(v, a, b => {
-                match b.to_i32() {
-                    Some(b_int) => {
-                        if a.is_zero() && b_int < 0 {
-                            None
-                        } else {
-                            Some(a.pow(b_int))
-                        }
-                    }
-                    None => None
-                }
-
-            }),
-            Math::Reciprocal(a) => map!(v, a => {
-                if a.is_zero() {
-                    None
-                } else {
-                    Some(a.recip())
                 }
             }),
         }
@@ -112,14 +78,8 @@ impl SynthLanguage for Math {
         let mut consts: Vec<Option<Constant>> = vec![];
 
         for i in 0..synth.params.important_cvec_offsets {
-            consts.push(mk_constant(
-                &i.to_bigint().unwrap(),
-                &(1.to_bigint().unwrap()),
-            ));
-            consts.push(mk_constant(
-                &(-i.to_bigint().unwrap()),
-                &(1.to_bigint().unwrap()),
-            ));
+            consts.push(Some(i.to_bigint().unwrap()));
+            consts.push(Some(-i.to_bigint().unwrap()));
         }
 
         consts.sort();
@@ -129,14 +89,11 @@ impl SynthLanguage for Math {
         // add the necessary random values, if any
         for row in consts.iter_mut() {
             let n_samples = synth.params.n_samples;
-            let svals: Vec<Constant> = sampler(&mut synth.rng, 8, 6, n_samples);
+            let svals: Vec<Constant> = sampler(&mut synth.rng, 8, n_samples);
             let mut vals: Vec<Option<Constant>> = vec![];
             for v in svals {
                 vals.push(Some(v));
             }
-            // let vals: Vec<Option<Constant>> = (0..n_samples)
-            //     .map(|_| mk_constant(&synth.rng.gen_bigint(32), &gen_denom(&mut synth.rng, 32)))
-            //     .collect();
             row.extend(vals);
         }
 
@@ -167,14 +124,11 @@ impl SynthLanguage for Math {
                 to_add.push(Math::Sub([i, j]));
                 to_add.push(Math::Mul([i, j]));
                 to_add.push(Math::Div([i, j]));
-                // to_add.push(Math::Pow([i, j]));
             }
             if synth.egraph[i].data.exact {
                 continue;
             }
-            to_add.push(Math::Abs(i));
             to_add.push(Math::Neg(i));
-            // to_add.push(Math::Reciprocal(i));
         }
 
         log::info!("Made a layer of {} enodes", to_add.len());
@@ -200,12 +154,12 @@ impl SynthLanguage for Math {
                 // match solver.check() {
                 SatResult::Unsat => true,
                 SatResult::Sat => {
-                    // println!("z3 validation: failed for {} => {}", lhs, rhs);
+                    println!("z3 validation: failed for {} => {}", lhs, rhs);
                     false
                 }
                 SatResult::Unknown => {
                     synth.smt_unknown += 1;
-                    // println!("z3 validation: unknown for {} => {}", lhs, rhs);
+                    println!("z3 validation: unknown for {} => {}", lhs, rhs);
                     false
                 }
             }
@@ -223,14 +177,9 @@ impl SynthLanguage for Math {
 
             for cvec in env.values_mut() {
                 cvec.reserve(n);
-                for s in sampler(&mut synth.rng, 8, 6, n) {
+                for s in sampler(&mut synth.rng, 8, n) {
                     cvec.push(Some(s));
                 }
-                // for _ in 0..n {
-                //     let numer = synth.rng.gen_bigint(32);
-                //     let denom = gen_denom(&mut synth.rng, 32);
-                //     cvec.push(Some(Ratio::new(numer, denom)));
-                // }
             }
 
             let lvec = Self::eval_pattern(lhs, &env, n);
@@ -241,30 +190,10 @@ impl SynthLanguage for Math {
     }
 }
 
-// randomly sample so that they are not 0
-// Ratio::new will panic if the denom is 0
-pub fn gen_pos(rng: &mut Pcg64, bits: u64) -> BigInt {
-    let mut res: BigInt;
-    loop {
-        res = rng.gen_bigint(bits);
-        if res != 0.to_bigint().unwrap() {
-            break;
-        }
-    }
-    res
-}
-
-pub fn sampler(rng: &mut Pcg64, b1: u64, b2: u64, num_samples: usize) -> Vec<Ratio<BigInt>> {
+pub fn sampler(rng: &mut Pcg64, nbits: u64, num_samples: usize) -> Vec<BigInt> {
     let mut ret = vec![];
     for _ in 0..num_samples {
-        let big = gen_pos(rng, b1);
-        let small = gen_pos(rng, b2);
-        let flip = rng.gen::<bool>();
-        if flip {
-            ret.push(Ratio::new(big, small));
-        } else {
-            ret.push(Ratio::new(small, big))
-        }
+        ret.push(rng.gen_bigint(nbits));
     }
     ret
 }
@@ -272,53 +201,39 @@ pub fn sampler(rng: &mut Pcg64, b1: u64, b2: u64, num_samples: usize) -> Vec<Rat
 fn egg_to_z3<'a>(
     ctx: &'a z3::Context,
     expr: &[Math],
-) -> (z3::ast::Real<'a>, Vec<z3::ast::Bool<'a>>) {
-    let mut buf: Vec<z3::ast::Real> = vec![];
+) -> (z3::ast::Int<'a>, Vec<z3::ast::Bool<'a>>) {
+    let mut buf: Vec<z3::ast::Int> = vec![];
     let mut assumes: Vec<z3::ast::Bool> = vec![];
-    let zero = z3::ast::Real::from_real(&ctx, 0, 1);
+    let zero = z3::ast::Int::from_i64(&ctx, 0);
     for node in expr.as_ref().iter() {
         match node {
-            Math::Var(v) => buf.push(z3::ast::Real::new_const(&ctx, v.to_string())),
-            Math::Num(c) => buf.push(z3::ast::Real::from_real(
-                &ctx,
-                (c.numer()).to_i32().unwrap(),
-                (c.denom()).to_i32().unwrap(),
-                // to_i32(c.numer()),
-                // to_i32(c.denom()),
-            )),
-            Math::Add([a, b]) => buf.push(z3::ast::Real::add(
+            Math::Var(v) => buf.push(z3::ast::Int::new_const(&ctx, v.to_string())),
+            Math::Num(c) => buf.push(z3::ast::Int::from_str(&ctx, &c.to_string()).unwrap()),
+            Math::Add([a, b]) => buf.push(z3::ast::Int::add(
                 &ctx,
                 &[&buf[usize::from(*a)], &buf[usize::from(*b)]],
             )),
-            Math::Sub([a, b]) => buf.push(z3::ast::Real::sub(
+            Math::Sub([a, b]) => buf.push(z3::ast::Int::sub(
                 &ctx,
                 &[&buf[usize::from(*a)], &buf[usize::from(*b)]],
             )),
-            Math::Mul([a, b]) => buf.push(z3::ast::Real::mul(
+            Math::Mul([a, b]) => buf.push(z3::ast::Int::mul(
                 &ctx,
                 &[&buf[usize::from(*a)], &buf[usize::from(*b)]],
             )),
             Math::Div([a, b]) => {
                 let denom = &buf[usize::from(*b)];
-                let lez = z3::ast::Real::le(denom, &zero);
-                let gez = z3::ast::Real::ge(denom, &zero);
+                let lez = z3::ast::Int::le(denom, &zero);
+                let gez = z3::ast::Int::ge(denom, &zero);
                 let assume = z3::ast::Bool::not(&z3::ast::Bool::and(&ctx, &[&lez, &gez]));
                 assumes.push(assume);
-                buf.push(z3::ast::Real::div(
+                buf.push(z3::ast::Int::div(
                     &buf[usize::from(*a)],
                     &buf[usize::from(*b)],
                 ))
             }
-            Math::Neg(a) => buf.push(z3::ast::Real::unary_minus(&buf[usize::from(*a)])),
-            Math::Abs(a) => {
-                let inner = &buf[usize::from(*a)].clone();
-                buf.push(z3::ast::Bool::ite(
-                    &z3::ast::Real::le(inner, &zero),
-                    &z3::ast::Real::unary_minus(inner),
-                    &inner,
-                ));
-            }
-            _ => unimplemented!(),
+            Math::Neg(a) => buf.push(z3::ast::Int::unary_minus(&buf[usize::from(*a)])),
+            // _ => unimplemented!(),
         }
     }
     (buf.pop().unwrap(), assumes)
