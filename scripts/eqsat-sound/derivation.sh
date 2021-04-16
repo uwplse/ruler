b918285c52e5f3a5a3689ecfd64f72e607ce2327
Chandrakana Nandi <cnandi@cs.washington.edu>
2021-04-15 19:25:27 -0700

    a hacky derivation thing

--- scripts/eqsat-sound/derivation.sh
+++ scripts/eqsat-sound/derivation.sh
@@ -18,20 +18,19 @@ mkdir -p "$DIR"
 
 is_first_rs=true
 prev_rs=""
-for rs in $(find "$1" -name 'out.json' | sort); do
+for rs in $(find $1 -name 'out.json' | sort); do
     if [ "$(jq '.status' "$rs")" = "CRASH" ]; then
         echo "WARNING: SKIPPING CRASHED RULESET!"
         echo "$rs"
         echo
         continue
-    fi
+    fi  
     if $is_first_rs; then
         prev_rs="$rs"
         is_first_rs=false
         continue
     fi
-
-    # MYDIR/output/2021-04-12_1705/32-fuzz-1000-samples-343/out.json
+    
 
     IFS='/ ' read -r -a array1 <<< "$prev_rs"
     IFS='/ ' read -r -a array2 <<< "$rs"
@@ -39,40 +38,46 @@ for rs in $(find "$1" -name 'out.json' | sort); do
     # indexing like this will break if directory structure changes.
     name1="${array1[-2]}"
     name2="${array2[-2]}"
-
+    echo "$name1"
+    echo "$name2"
     IFS='- ' read -r -a dm1 <<< "$name1"
     IFS='- ' read -r -a dm2 <<< "$name2"
 
     dom1="${dm1[0]}"
     dom2="${dm2[0]}"
-
+    echo "$dom1"
+    echo "$dom2"
+        
     if [ "$dom1" = "4" ] || [ "$dom1" = "32" ]; then
         d1="bv$dom1"
     else
         d1="$2"
-    fi
+    fi  
     if [ "$dom2" = "4" ] || [ "$dom2" = "32" ]; then
         d2="bv$dom2"
     else
         d2="$2"
-    fi
+    fi  
     if [[ "$d1" = "$2"  &&  "$d2" = "$2" ]]; then
         echo "PREVIOUS = $prev_rs"
         echo "CURRENT  = $rs"
         echo "Deriving $name1/out.json and $name2/out.json"
-
+        
         dout="$DIR/derive-$name1-$name2.json"
         echo "Sending output to '$dout'"
-        cargo run --bin "$2" --release -- derive "$prev_rs" "$rs" "$dout" 2> "$dout.stderr"
-
+        #cargo run --bin "$2" --release -- derive "$prev_rs" "$rs" "$dout" >&2 "$dout.stderr"
+        cargo run --bin "$2" --release -- derive "$prev_rs" "$rs" "$dout"
+            
         if [ "$(jq '.forward.not_derivable' "$dout")" != "[]" ]; then
             echo "FORWARD FAILED!!!"
             echo "Not derivable:"
             jq '.forward.not_derivable' "$dout"
-        fi
+            jq '.forward.not_derivable' "$dout" >> "$DIR/derive-$name1-$name2-forward.json"
+        fi  
         if [ "$(jq '.reverse.not_derivable' "$dout")" != "[]" ]; then
             echo "REVERSE FAILED!!!"
             jq '.reverse.not_derivable' "$dout"
+            jq '.reverse.not_derivable' "$dout" >> "$DIR/derive-$name1-$name2-reverse.json"
         fi
         echo
     fi
@@ -80,6 +85,7 @@ for rs in $(find "$1" -name 'out.json' | sort); do
     # update for next iter
     prev_rs="$rs"
 done
+
 
 
 # for out1 in $(find $1 -name 'out.json' | sort); do
