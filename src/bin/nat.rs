6f6515d1915e28d666f5d71ffc7ed8ab301f657b
anjali <anjalip@cs.washington.edu>
2022-08-19 11:57:35 -0700

    fill in nat impl

--- src/bin/nat.rs
+++ src/bin/nat.rs
@@ -1,5 +1,6 @@
 use egg::*;
-use ruler::*;
+use rand::Rng;
+use ruler::{HashMap, *};
 
 define_language! {
     pub enum Nat {
@@ -13,7 +14,7 @@ define_language! {
 
 impl std::fmt::Display for Type {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        todo!()
+        write!(f, "")
     }
 }
 
@@ -22,43 +23,131 @@ pub enum Type {
     Nat,
 }
 
+impl Nat {
+    fn mk_constant_id(c: usize, egraph: &mut EGraph<Self, SynthAnalysis>) -> Id {
+        match c {
+            0 => egraph.add(Nat::Z),
+            _ => {
+                let pred = Self::mk_constant_id(c - 1, egraph);
+                egraph.add(Nat::S(pred))
+            }
+        }
+    }
+}
+
 impl SynthLanguage for Nat {
     type Constant = usize;
 
     type Type = Type;
 
     fn get_type(&self) -> Self::Type {
-        todo!()
+        Type::Nat
     }
 
-    fn eval<'a, F>(&'a self, cvec_len: usize, f: F) -> CVec<Self>
+    fn eval<'a, F>(&'a self, cvec_len: usize, mut v: F) -> CVec<Self>
     where
         F: FnMut(&'a Id) -> &'a CVec<Self>,
     {
-        todo!()
+        match self {
+            Nat::Z => vec![Some(0); cvec_len],
+            Nat::S(x) => map!(v, x => Some(*x + 1)),
+            Nat::Add([a, b]) => map!(v, a, b => Some(*a + *b)),
+            Nat::Mul([a, b]) => map!(v, a, b => Some(*a * *b)),
+            Nat::Var(_) => vec![],
+        }
+    }
+
+    fn mk_interval(&self, egraph: &EGraph<Self, SynthAnalysis>) -> Interval<Self::Constant> {
+        match self {
+            Nat::Z => Interval::new(Some(0), Some(0)),
+            Nat::S(x) => {
+                let x_int = egraph[*x].data.interval.clone();
+                if let Some(l) = x_int.low {
+                    Interval::new(Some(l + 1), x_int.high.map(|v| v + 1))
+                } else {
+                    panic!("There shouldn't be infinite lower bounds for Nat")
+                }
+            }
+            Nat::Add([x, y]) => {
+                let x_int = egraph[*x].data.interval.clone();
+                let y_int = egraph[*y].data.interval.clone();
+
+                let low = x_int
+                    .low
+                    .zip(y_int.low)
+                    .map(|(a, b)| a + b)
+                    .expect("There shouldn't be infinite lower bounds for Nat");
+
+                let high = x_int.high.zip(y_int.high).map(|(a, b)| a + b);
+
+                Interval::new(Some(low), high)
+            }
+            Nat::Mul([x, y]) => {
+                let x_int = egraph[*x].data.interval.clone();
+                let y_int = egraph[*y].data.interval.clone();
+
+                let low = x_int
+                    .low
+                    .zip(y_int.low)
+                    .map(|(a, b)| a * b)
+                    .expect("There shouldn't be infinite lower bounds for Nat");
+
+                let high = x_int.high.zip(y_int.high).map(|(a, b)| a * b);
+
+                Interval::new(Some(low), high)
+            }
+            Nat::Var(_) => Interval::new(Some(0), None),
+        }
     }
 
     fn to_var(&self) -> Option<Symbol> {
-        todo!()
+        if let Nat::Var(sym) = self {
+            Some(*sym)
+        } else {
+            None
+        }
     }
 
     fn mk_var(sym: egg::Symbol) -> Self {
-        todo!()
+        Nat::Var(sym)
     }
 
-        todo!()
-    fn mk_constant(c: Self::Constant, _egraph: &mut EGraph<Self, SynthAnalysis>) -> Self {
+    fn mk_constant(c: Self::Constant, egraph: &mut EGraph<Self, SynthAnalysis>) -> Self {
+        match c {
+            0 => Nat::Z,
+            _ => Nat::S(Self::mk_constant_id(c - 1, egraph)),
+        }
     }
 
     fn is_constant(&self) -> bool {
-        todo!()
+        match self {
+            Nat::Z => true,
+            _ => false,
+        }
     }
 
     fn init_synth(synth: &mut Synthesizer<Self>) {
-        todo!()
+        let cvec_len = 10;
+        let mut egraph: EGraph<Nat, SynthAnalysis> = EGraph::new(SynthAnalysis {
+            cvec_len: cvec_len,
+            constant_fold: ConstantFoldMethod::IntervalAnalysis,
+            rule_lifting: false,
+        });
+
+        for i in 0..synth.params.variables {
+            let var = Symbol::from(letter(i));
+            let id = egraph.add(Nat::Var(var));
+            let mut vals = vec![];
+            for _ in 0..cvec_len {
+                vals.push(Some(synth.rng.gen::<usize>()));
+            }
+            egraph[id].data.cvec = vals.clone();
+        }
+
+        synth.egraph = egraph;
     }
 
-    fn make_layer(synth: &Synthesizer<Self>, iter: usize) -> Vec<Self> {
+    fn make_layer(_synth: &Synthesizer<Self>, _iter: usize) -> Vec<Self> {
         todo!()
     }
 
@@ -67,7 +156,28 @@ impl SynthLanguage for Nat {
         lhs: &Pattern<Self>,
         rhs: &Pattern<Self>,
     ) -> ValidationResult {
-        todo!()
+        let n = synth.params.num_fuzz;
+        let mut env = HashMap::default();
+        for var in lhs.vars() {
+            env.insert(var, vec![]);
+        }
+        for var in rhs.vars() {
+            env.insert(var, vec![]);
+        }
+
+        for cvec in env.values_mut() {
+            cvec.reserve(n);
+            let mut vals = vec![];
+            for _ in 0..n {
+                vals.push(synth.rng.gen::<usize>());
+            }
+            for v in vals {
+                cvec.push(Some(v));
+            }
+        }
+        let lvec = Self::eval_pattern(lhs, &env, n);
+        let rvec = Self::eval_pattern(rhs, &env, n);
+        ValidationResult::from(lvec == rvec)
     }
 }
 
