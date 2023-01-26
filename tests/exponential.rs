5deb1bee3d23707d180450f46ffea3bf4288a91c
Brett Saiki <bksaiki@gmail.com>
2023-01-26 15:34:13 -0800

    exponential recipe

--- tests/exponential.rs
+++ tests/exponential.rs
@@ -36,12 +36,22 @@ impl SynthLanguage for Exponential {
     // cvec-less domain
     fn eval<'a, F>(&'a self, _cvec_len: usize, _get_cvec: F) -> CVec<Self>
     where
-        F: FnMut(&'a Id) -> &'a CVec<Self> {
+        F: FnMut(&'a Id) -> &'a CVec<Self>,
+    {
         vec![]
     }
 
-    // no variable initialization required
-    fn initialize_vars(_egraph: &mut EGraph<Self, SynthAnalysis>, _vars: &[String]) {}
+    fn initialize_vars(egraph: &mut EGraph<Self, SynthAnalysis>, vars: &[String]) {
+        for v in vars {
+            let id = egraph.add(Exponential::Var(Symbol::from(v.clone())));
+
+            let l_id = egraph.add(Exponential::Log(id));
+            egraph.add(Exponential::Exp(l_id));
+
+            let e_id = egraph.add(Exponential::Exp(id));
+            egraph.add(Exponential::Log(e_id));
+        }
+    }
 
     fn to_var(&self) -> Option<Symbol> {
         if let Exponential::Var(sym) = self {
@@ -92,13 +102,256 @@ impl SynthLanguage for Exponential {
             // inverse properties
             "(log (exp ?a)) ==> ?a",
             "(exp (log ?a)) ==> ?a",
-            // inverse (non-trivial applier)
-            // "?x" => "(log (exp ?x))" if is_var_str("?x"),
-            // "?x" => "(exp (log ?x))" if is_var_str("?x"),
         ])
     }
 
     fn is_allowed_op(&self) -> bool {
         true
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use ruler::enumo;
+
+    type Workload = enumo::Workload;
+    type Ruleset = enumo::Ruleset<Exponential>;
+    type Filter = enumo::Filter;
+
+    macro_rules! str_vec {
+        ($($x:expr),*) => (vec![$($x.to_string()),*]);
+    }
+
+    // TODO: actually derive these rules
+    fn rational_rules() -> Ruleset {
+        Ruleset::from_str_vec(&[
+            "(* ?c (* ?b ?a)) ==> (* ?b (* ?a ?c))",
+            "(* ?b (* ?a ?c)) ==> (* ?c (* ?b ?a))",
+            "(+ ?c (+ ?b ?a)) ==> (+ ?a (+ ?b ?c))",
+            "(/ (/ ?c ?b) ?a) ==> (/ (/ ?c ?a) ?b)",
+            "(- (- ?c ?b) ?a) ==> (- (- ?c ?a) ?b)",
+            "(/ ?c (/ ?b ?a)) ==> (/ ?a (/ ?b ?c))",
+            "(/ ?c (* ?b ?a)) ==> (/ (/ ?c ?a) ?b)",
+            "(/ (/ ?c ?a) ?b) ==> (/ ?c (* ?b ?a))",
+            "(- ?c (+ ?b ?a)) ==> (- (- ?c ?a) ?b)",
+            "(- (- ?c ?a) ?b) ==> (- ?c (+ ?b ?a))",
+            "(/ (* ?c ?b) ?a) ==> (* ?c (/ ?b ?a))",
+            "(* ?c (/ ?b ?a)) ==> (/ (* ?c ?b) ?a)",
+            "(/ ?c (/ ?b ?a)) ==> (* ?c (/ ?a ?b))",
+            "(* ?c (/ ?a ?b)) ==> (/ ?c (/ ?b ?a))",
+            "(/ ?c (- ?b (- ?b ?a))) ==> (/ ?c ?a)",
+            "(/ (- ?c (- ?c ?b)) ?a) ==> (/ ?b ?a)",
+            "(+ (/ ?c ?a) (/ ?b ?a)) ==> (/ (+ ?c ?b) ?a)",
+            "(/ (+ ?c ?b) ?a) ==> (+ (/ ?c ?a) (/ ?b ?a))",
+            "(* ?a (- ?c ?b)) ==> (- (* ?a ?c) (* ?b ?a))",
+            "(- (* ?a ?c) (* ?b ?a)) ==> (* ?a (- ?c ?b))",
+            "(- (/ ?c ?a) (/ ?b ?a)) ==> (/ (- ?c ?b) ?a)",
+            "(/ (- ?c ?b) ?a) ==> (- (/ ?c ?a) (/ ?b ?a))",
+            "(* ?b (+ ?c ?a)) ==> (+ (* ?c ?b) (* ?b ?a))",
+            "(+ (* ?c ?b) (* ?b ?a)) ==> (* ?b (+ ?c ?a))",
+            "(/ (+ ?c (- ?a ?b)) ?a) ==> (/ (+ ?a (- ?c ?b)) ?a)",
+            "(/ (+ ?a (- ?c ?b)) ?a) ==> (/ (+ ?c (- ?a ?b)) ?a)",
+            "(/ (+ ?c (- 2 ?b)) ?a) ==> (/ (+ 2 (- ?c ?b)) ?a)",
+            "(/ (+ 2 (- ?c ?b)) ?a) ==> (/ (+ ?c (- 2 ?b)) ?a)",
+            "(+ ?b ?a) ==> (+ ?a ?b)",
+            "(* ?b ?a) ==> (* ?a ?b)",
+            "(- ?a ?b) ==> (~ (- ?b ?a))",
+            "(~ (- ?b ?a)) ==> (- ?a ?b)",
+            "(+ ?b (~ ?a)) ==> (- ?b ?a)",
+            "(- ?b ?a) ==> (+ ?b (~ ?a))",
+            "(* ?b (~ ?a)) ==> (~ (* ?b ?a))",
+            "(~ (* ?b ?a)) ==> (* ?b (~ ?a))",
+            "(/ (~ ?b) ?a) ==> (/ ?b (~ ?a))",
+            "(/ ?b (~ ?a)) ==> (/ (~ ?b) ?a)",
+            "(/ ?b (~ ?a)) ==> (~ (/ ?b ?a))",
+            "(~ (/ ?b ?a)) ==> (/ ?b (~ ?a))",
+            "(* ?a (/ ?b ?a)) ==> ?b",
+            "(* (- ?b ?a) (+ ?a ?b)) ==> (- (* ?b ?b) (* ?a ?a))",
+            "(- (* ?b ?b) (* ?a ?a)) ==> (* (- ?b ?a) (+ ?a ?b))",
+            "(/ (- ?a ?b) ?a) ==> (- 1 (/ ?b ?a))",
+            "(- 1 (/ ?b ?a)) ==> (/ (- ?a ?b) ?a)",
+            "(/ (+ ?a ?b) ?a) ==> (+ 1 (/ ?b ?a))",
+            "(+ 1 (/ ?b ?a)) ==> (/ (+ ?a ?b) ?a)",
+            "(- ?b (* ?b ?a)) ==> (* ?b (- 1 ?a))",
+            "(* ?b (- 1 ?a)) ==> (- ?b (* ?b ?a))",
+            "(+ ?a (* ?b ?a)) ==> (* ?a (+ ?b 1))",
+            "(* ?a (+ ?b 1)) ==> (+ ?a (* ?b ?a))",
+            "(/ ?b (+ ?b (/ ?b ?a))) ==> (/ ?a (+ ?a 1))",
+            "(/ ?a (- (/ ?a ?b) ?a)) ==> (/ ?b (- 1 ?b))",
+            "(/ ?a (- (* ?a ?b) ?a)) ==> (/ 1 (+ ?b -1))",
+            "?a ==> (~ (~ ?a))",
+            "(~ (~ ?a)) ==> ?a",
+            "?a ==> (- ?a 0)",
+            "(- ?a 0) ==> ?a",
+            "?a ==> (* ?a 1)",
+            "(* ?a 1) ==> ?a",
+            "?a ==> (/ ?a 1)",
+            "(/ ?a 1) ==> ?a",
+            "?a ==> (+ ?a 0)",
+            "(+ ?a 0) ==> ?a",
+            "(- ?a ?a) ==> 0",
+            "(/ ?a ?a) ==> 1",
+            "(~ ?a) ==> (- 0 ?a)",
+            "(- 0 ?a) ==> (~ ?a)",
+            "(~ ?a) ==> (/ ?a -1)",
+            "(/ ?a -1) ==> (~ ?a)",
+            "(~ ?a) ==> (* ?a -1)",
+            "(* ?a -1) ==> (~ ?a)",
+            "(+ ?a ?a) ==> (* ?a 2)",
+            "(* ?a 2) ==> (+ ?a ?a)",
+            "(/ 0 ?a) ==> 0",
+            "(* ?a 0) ==> 0",
+            "(- ?a 1) ==> (+ ?a -1)",
+            "(+ ?a -1) ==> (- ?a 1)",
+            "(- ?a -1) ==> (+ ?a 1)",
+            "(+ ?a 1) ==> (- ?a -1)",
+        ])
+    }
+
+    fn run_workload(terms: Workload, prev_rules: &Ruleset) -> Ruleset {
+        let rules = Exponential::run_workload(
+            terms,
+            prev_rules.clone(),
+            Limits {
+                iter: 3,
+                node: 2_000_000,
+            },
+        );
+
+        rules
+    }
+
+    fn constant_rules(prev_rules: &Ruleset) -> Ruleset {
+        let terms = Workload::from_vec(vec![
+            "(exp 0)",
+            "(exp 1)",
+            "(log 1)",
+            "(sqrt 1)",
+            "(cbrt 1)",
+            "(pow a 1)",
+            "(pow 1 a)",
+        ]);
+
+        run_workload(terms, prev_rules)
+    }
+
+    fn exp_rules(prev_rules: &Ruleset) -> Ruleset {
+        let lower_layer = Workload::from_vec(vec!["v", "(uop v)", "(bop v v)"])
+            .plug("v", &Workload::from_vec(vec!["a", "b", "c"]))
+            .plug("uop", &Workload::from_vec(vec!["exp"]))
+            .plug("bup", &Workload::from_vec(vec!["+", "*"]));
+
+        let upper_layer = Workload::from_vec(vec!["(uop v)", "(bop v v)"])
+            .plug("v", &lower_layer)
+            .plug("uop", &Workload::from_vec(vec!["exp"]))
+            .plug("bop", &Workload::from_vec(vec!["+", "*"]));
+        // TODO: inverted filters
+
+        run_workload(upper_layer, prev_rules)
+    }
+
+    fn log_rules(prev_rules: &Ruleset) -> Ruleset {
+        let lower_layer = Workload::from_vec(vec!["v", "(uop v)", "(bop v v)"])
+            .plug("v", &Workload::from_vec(vec!["a", "b", "c"]))
+            .plug("uop", &Workload::from_vec(vec!["log"]))
+            .plug("bup", &Workload::from_vec(vec!["+"]));
+
+        let upper_layer = Workload::from_vec(vec!["(uop v)", "(bop v v)"])
+            .plug("v", &lower_layer)
+            .plug("uop", &Workload::from_vec(vec!["log"]))
+            .plug("bop", &Workload::from_vec(vec!["*"]));
+        // TODO: inverted filters
+
+        run_workload(upper_layer, prev_rules)
+    }
+
+    fn no_pow_rules(prev_rules: &Ruleset) -> Ruleset {
+        let vars = Workload::from_vec(vec!["a", "b", "c"]);
+        let uops = Workload::from_vec(vec!["exp", "log", "sqrt", "cbrt"]);
+        let bops = Workload::from_vec(vec!["+", "*"]);
+        let lang = Workload::from_vec(vec!["v", "(uop v)", "(bop v v)"]);
+
+        let lower_layer = lang.clone()
+            .plug("v", &vars)
+            .plug("uop", &uops)
+            .plug("bup", &bops);
+
+        let upper_layer = lang
+            .plug("v", &Workload::Append(vec![lower_layer, vars]))
+            .plug("uop", &uops)
+            .plug("bop", &bops);
+        // TODO: inverted filters
+
+        run_workload(upper_layer, prev_rules)
+    }
+
+    fn simple_rules(prev_rules: &Ruleset) -> Ruleset {
+        let vars = Workload::from_vec(vec!["a", "b", "c"]);
+        let uops = Workload::from_vec(vec!["exp", "log", "sqrt", "cbrt"]);
+        let bops = Workload::from_vec(vec!["+", "*", "pow"]);
+        let lang = Workload::from_vec(vec!["v", "(uop v)", "(bop v v)"]);
+
+        let lower_layer = lang.clone()
+            .plug("v", &vars)
+            .plug("uop", &uops)
+            .plug("bup", &bops);
+
+        let upper_layer = lang
+            .plug("v", &Workload::Append(vec![lower_layer, vars]))
+            .plug("uop", &uops)
+            .plug("bop", &bops);
+        // TODO: inverted filters
+
+        run_workload(upper_layer, prev_rules)
+    }
+
+    fn div_rules(prev_rules: &Ruleset) -> Ruleset {
+        let vars = Workload::from_vec(vec!["a", "b"]);
+        let uops = Workload::from_vec(vec!["sqrt", "cbrt"]);
+        let bops = Workload::from_vec(vec!["/"]);
+        let lang = Workload::from_vec(vec!["v", "(uop v)", "(bop v v)"]);
+
+        let lower_layer = lang.clone()
+            .plug("v", &vars)
+            .plug("uop", &uops)
+            .plug("bup", &bops);
+
+        let upper_layer = lang
+            .plug("v", &Workload::Append(vec![lower_layer, vars]))
+            .plug("uop", &uops)
+            .plug("bop", &bops)
+            .filter(Filter::Canon(str_vec!["a", "b"]));
+
+        run_workload(upper_layer, prev_rules)
+    }
+
+    #[test]
+    fn make_rules() {
+        let mut all_rules = rational_rules();
+
+        // Constant layer
+        let const_rules = constant_rules(&all_rules);
+        all_rules.extend(const_rules);
+
+        // Exponential layer
+        let exp_rules = exp_rules(&all_rules);
+        all_rules.extend(exp_rules);
+
+        // Logarithm layer
+        let log_rules = log_rules(&all_rules);
+        all_rules.extend(log_rules);
+
+        // No `pow` layer
+        let no_pow_rules = no_pow_rules(&all_rules);
+        all_rules.extend(no_pow_rules);
+
+        // Simple layer
+        let simple_rules = simple_rules(&all_rules);
+        all_rules.extend(simple_rules);
+
+        // div layer
+        let div_rules = div_rules(&all_rules);
+        all_rules.extend(div_rules);
     }
 }
