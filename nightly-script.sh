3c1d892c13c7ac666d12f3dd1312b902dbdeffe2
vee-effekt <gannet@cs.washington.edu>
2023-01-27 00:50:15 -0800

    Rep.

--- nightly-script.sh
+++ nightly-script.sh
@@ -14,7 +14,6 @@ B=$(git rev-parse --abbrev-ref HEAD)
 C=$(git rev-parse HEAD | sed 's/\(..........\).*/\1/')
 RDIR="$(date +%s):$(hostname):$B:$C"
 
-nightly-results publish --name
 nightly-results publish --name "$RDIR" "$DIR"
 
 rm output.json
