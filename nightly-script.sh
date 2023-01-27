6635f1c169457300c2595c7056f77701b66a7f94
vee-effekt <gannet@cs.washington.edu>
2023-01-27 00:37:01 -0800

    Testing.

--- nightly-script.sh
+++ nightly-script.sh
@@ -5,9 +5,16 @@ cargo test
 echo -n "]" >> output.json
 
 OUTPUT=`cat output.json`
-sed -i "204 i var obj = ${OUTPUT};" index.html
+echo 
+sed -i "204 i var obj = ${OUTPUT};" rep/index.html
+echo "echo ${OUTPUT}"
 
-nightly-results publish --name output.json output.json
-nightly-results publish --name index.html index.html
+DIR="rep"
+B=$(git rev-parse --abbrev-ref HEAD)
+C=$(git rev-parse HEAD | sed 's/\(..........\).*/\1/')
+RDIR="$(date +%s):$(hostname):$B:$C"
+
+nightly-results publish --name
+nightly-results publish --name "$RDIR" "$DIR"
 
 rm output.json
