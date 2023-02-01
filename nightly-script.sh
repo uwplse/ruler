e0cc8287919245b50f5be13146894c29c416f680
vee-effekt <gannet@cs.washington.edu>
2023-01-31 19:37:03 -0800

    Oops.

--- nightly-script.sh
+++ nightly-script.sh
@@ -19,20 +19,20 @@ cargo test
 echo -n "]" >> rep/json/output.json
 
 OUTPUT=`cat rep/json/output.json`
-sed "8 i var obj = ${OUTPUT};" rep/index_base.html > rep/index.html
+sed "11 i var obj = ${OUTPUT};" rep/index_base.html > rep/index.html
 echo "echo ${OUTPUT}"
 
 BOOL=`cat rep/json/bool.json`
-sed "13 i var obj = ${BOOL};" rep/bool_base.html > rep/bool.html
+sed "14 i var obj = ${BOOL};" rep/bool_base.html > rep/bool.html
 
 BV32=`cat rep/json/bv32.json`
-sed "13 i var obj = ${BV32};" rep/bv32_base.html > rep/bv32.html
+sed "14 i var obj = ${BV32};" rep/bv32_base.html > rep/bv32.html
 
 BV4=`cat rep/json/bv4.json`
-sed "13 i var obj = ${BV4};" rep/bv4_base.html > rep/bv4.html
+sed "14 i var obj = ${BV4};" rep/bv4_base.html > rep/bv4.html
 
 RATIONAL=`cat rep/json/bool.json`
-sed "13 i var obj = ${RATIONAL};" rep/rational_base.html > rep/rational.html
+sed "14 i var obj = ${RATIONAL};" rep/rational_base.html > rep/rational.html
 
 DIR="rep"
 B=$(git rev-parse --abbrev-ref HEAD)
