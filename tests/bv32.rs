c3b77567116d2762ddae1766e3a515d35b78812c
Anjali Pal <anjalip@cs.washington.edu>
2023-03-16 14:02:55 -0700

    rename Metric::List -> Lists

--- tests/bv32.rs
+++ tests/bv32.rs
@@ -51,14 +51,14 @@ mod test {
         let bops = Workload::new(["&", "|", "*", "--", "+"]);
 
         let layer_1 = Workload::make_layer(initial_vals.clone(), uops.clone(), bops.clone())
-            .filter(Filter::MetricLt(Metric::List, 2));
+            .filter(Filter::MetricLt(Metric::Lists, 2));
         let terms_1 = layer_1.clone().append(initial_vals.clone());
         let rules_1 = Bv::run_workload(terms_1.clone(), all_rules.clone(), Limits::default());
         all_rules.extend(rules_1.clone());
 
         let layer_2 = Workload::make_layer(layer_1.clone(), uops.clone(), bops.clone())
-            .filter(Filter::MetricLt(Metric::List, 3))
-            .filter(Filter::Invert(Box::new(Filter::MetricLt(Metric::List, 1))));
+            .filter(Filter::MetricLt(Metric::Lists, 3))
+            .filter(Filter::Invert(Box::new(Filter::MetricLt(Metric::Lists, 1))));
         let terms_2 = layer_2.clone().append(terms_1.clone());
         let rules_2 = Bv::run_workload(terms_2.clone(), all_rules.clone(), Limits::default());
         all_rules.extend(rules_2.clone());
