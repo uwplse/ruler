edd7a6ce6e88a4b71e66ee55921fd5ae0a007244
Brett Saiki <bksaiki@gmail.com>
2023-03-26 16:57:43 -0700

    first attempt

--- nightly-script.sh
+++ nightly-script.sh
@@ -3,9 +3,12 @@
 # the results to the nightly-results server. It also generates some HTML
 # to display the equiderivability tests in a nicely-formatted way.
 
-rm -rf rep/json/*
-rm -rf rep/output/*
-mkdir rep/output
+# Clean output directories
+rm -rf rep/json rep/output
+
+# Prepare output directories
+mkdir -p rep/output
+mkdir -p rep/json
 
 # Run tests.
 RUST_TEST_THREADS=1 cargo test --release
@@ -13,6 +16,9 @@ RUST_TEST_THREADS=1 cargo test --release
 # Update HTML index page.
 cp rep/*.js rep/output
 python3 generatehtml.py
+
+# Move json
+mv rep/json rep/output/json
 
 # This is the uploading part, copied directly from Herbie's nightly script.
 DIR="rep/output"
