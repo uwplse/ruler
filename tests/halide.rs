d852bffa6b40d8e528de28bab7a705a235d71040
Anjali Pal <anjalip@cs.washington.edu>
2023-04-04 23:41:47 -0700

    update baseline

--- tests/halide.rs
+++ tests/halide.rs
@@ -341,11 +341,17 @@ mod test {
         );
 
         // oopsla-halide-baseline branch
-        // Run on leviathan 3/31/2023
+        // Run on leviathan 4/4/2023
         // time cargo run --release --bin halide -- synth --iters 1 --use-smt
-        // real	0m3.354s
-        // user	0m3.274s
-        // sys	0m0.076s
+        // real	0m6.829s
+        // user	0m19.784s
+        // sys	0m0.595s
+
+        // With only Add, Sub, Mul, Div, Neg nodes added in make_layer
+        // time cargo run --release --bin halide -- synth --iters 2 --use-smt
+        // real	0m53.816s
+        // user	1m6.082s
+        // sys	0m1.259s
         let oopsla_halide: Ruleset<Pred> = Ruleset::from_file("baseline/oopsla-halide.rules");
         let oopsla_duration = Duration::from_secs_f32(3.354);
 
