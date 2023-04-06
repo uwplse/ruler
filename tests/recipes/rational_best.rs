3db0e2902b1256ae3b03e80027e9f832d517b7c8
Anjali Pal <anjalip@cs.washington.edu>
2023-04-05 21:32:56 -0700

    start best from basic

--- tests/recipes/rational_best.rs
+++ tests/recipes/rational_best.rs
@@ -1,43 +1,13 @@
+use crate::rational_replicate::replicate_ruler1_recipe;
+
 use super::*;
-use ruler::{
-    enumo::{Filter, Ruleset, Workload},
-    recipe_utils::{base_lang, iter_metric},
-};
+use ruler::enumo::{Filter, Ruleset, Workload};
 
 pub fn best_enumo_recipe() -> Ruleset<Math> {
-    let mut rules = Ruleset::default();
     let limits = Limits::default();
 
-    // Domain
-    let vars = &Workload::new(["a", "b", "c"]);
-    let consts = &Workload::new(["0", "-1", "1"]);
-    let uops = &Workload::new(["~", "fabs"]);
-    let bops = &Workload::new(["+", "-", "*", "/"]);
-
-    // Layer 1 (one op)
-    println!("layer1");
-    let layer1 = iter_metric(base_lang(), "EXPR", enumo::Metric::Depth, 2)
-        .filter(Filter::Contains("var".parse().unwrap()))
-        .plug("CONST", consts)
-        .plug("VAR", vars)
-        .plug("UOP", uops)
-        .plug("BOP", bops)
-        .plug("TOP", &Workload::empty());
-    let layer1_rules = Math::run_workload_conditional(layer1.clone(), rules.clone(), limits, false);
-    rules.extend(layer1_rules);
-
-    // Layer 2 (two ops)
-    println!("layer2");
-    let layer2 = iter_metric(base_lang(), "EXPR", enumo::Metric::Depth, 3)
-        .filter(Filter::Contains("var".parse().unwrap()))
-        .plug("CONST", consts)
-        .plug("VAR", vars)
-        .plug("UOP", uops)
-        .plug("BOP", bops)
-        .plug("TOP", &Workload::empty());
-    layer2.to_file("replicate_layer2_terms");
-    let layer2_rules = Math::run_workload_conditional(layer2.clone(), rules.clone(), limits, true);
-    rules.extend(layer2_rules);
+    // Start from the "basic" rational rules
+    let mut rules = replicate_ruler1_recipe();
 
     // Contains var filter
     let contains_var_filter = Filter::Or(vec![
