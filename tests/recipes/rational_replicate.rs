a02c45245c094bc84da5f05bf6420273c16e6870
Anjali Pal <anjalip@cs.washington.edu>
2023-04-05 19:50:00 -0700

    clean up replicate

--- tests/recipes/rational_replicate.rs
+++ tests/recipes/rational_replicate.rs
@@ -13,28 +13,38 @@ pub fn replicate_ruler1_recipe() -> Ruleset<Math> {
     let consts = &Workload::new(["0", "-1", "1"]);
     let uops = &Workload::new(["~", "fabs"]);
     let bops = &Workload::new(["+", "-", "*", "/"]);
+    let lang = base_lang()
+        .filter(Filter::Invert(Box::new(Filter::Contains(
+            "TOP".parse().unwrap(),
+        ))))
+        .plug("UOP", uops)
+        .plug("BOP", bops);
 
     // Layer 1 (one op)
     println!("layer1");
-    let layer1 = iter_metric(base_lang(), "EXPR", enumo::Metric::Depth, 2)
-        .filter(Filter::Contains("VAR".parse().unwrap()))
+    let layer1 = lang
+        .clone()
         .plug("CONST", consts)
         .plug("VAR", vars)
-        .plug("UOP", uops)
-        .plug("BOP", bops)
-        .plug("TOP", &Workload::empty());
+        .plug("EXPR", &vars.clone().append(consts.clone()));
+
     let layer1_rules = run_workload(layer1.clone(), rules.clone(), limits, false);
     rules.extend(layer1_rules);
 
     // Layer 2 (two ops)
     println!("layer2");
-    let layer2 = iter_metric(base_lang(), "EXPR", enumo::Metric::Depth, 3)
-        .filter(Filter::Contains("VAR".parse().unwrap()))
-        .plug("CONST", consts)
-        .plug("VAR", vars)
-        .plug("UOP", uops)
-        .plug("BOP", bops)
-        .plug("TOP", &Workload::empty());
+    let filter = Filter::Or(vec![
+        Filter::Contains("a".parse().unwrap()),
+        Filter::Contains("b".parse().unwrap()),
+        Filter::Contains("c".parse().unwrap()),
+    ]);
+    let layer2 = lang
+        .clone()
+        .plug("EXPR", &layer1)
+        .filter(filter)
+        .plug("CONST", &Workload::empty())
+        .plug("VAR", &Workload::empty());
+    layer2.to_file("l2.terms");
     let layer2_rules = run_workload(layer2.clone(), rules.clone(), limits, true);
     rules.extend(layer2_rules);
 
