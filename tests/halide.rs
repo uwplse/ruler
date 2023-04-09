7ced5995c61da436e5a197c21dc58040786e41ea
vee-effekt <gannet@cs.washington.edu>
2023-04-08 20:58:45 -0700

    No more halide it's too big

--- tests/halide.rs
+++ tests/halide.rs
@@ -300,13 +300,6 @@ mod test {
             return;
         }
 
-        let baseline: Ruleset<Pred> = Ruleset::from_file("baseline/halide.rules");
-        let start = Instant::now();
-        let all_rules = halide_rules();
-        let duration = start.elapsed();
-
-        logger::write_output(&all_rules, &baseline, "halide", "halide", duration);
-
         // oopsla-halide-baseline branch
         // Run on leviathan 4/4/2023
         // time cargo run --release --bin halide -- synth --iters 1 --use-smt
