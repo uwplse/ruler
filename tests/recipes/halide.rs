3ac0d33fbf69dc35f60321923b05668f987ffbf1
vee-effekt <gannet@cs.washington.edu>
2023-04-11 14:25:51 -0700

    LHS 57%, LHS/RHS 64%.

--- tests/recipes/halide.rs
+++ tests/recipes/halide.rs
@@ -10,7 +10,7 @@ pub fn halide_rules() -> Ruleset<Pred> {
     // This is porting the halide recipe at incremental/halide.spec
     // on the branch "maybe-useful" in the old recipes repo
     let mut all_rules = Ruleset::default();
-
+    /*
     // Bool rules up to size 5:
     let bool_only = recursive_rules(
         Metric::Atoms,
@@ -71,12 +71,39 @@ pub fn halide_rules() -> Ruleset<Pred> {
         ),
         all_rules.clone(),
     );
+    */
+    let full: Ruleset<Pred> = Ruleset::from_file("thru_full.rules");
     all_rules.extend(full);
-    let nested_bops_full = Workload::new(&["(bop e e)", "v", "0", "1"])
+    let mut full_ruleset = Ruleset::default();
+    full_ruleset.extend(all_rules.clone());
+    
+    println!("full finished.");
+    all_rules.to_file("thru_full.rules");
+
+    let nested_bops_arith = Workload::new(&["(bop e e)", "v"])
+        .plug("e", &Workload::new(&["(bop v v)", "(uop v)", "v"]))
+        .plug("bop", &Workload::new(&["+", "-", "*", "<", "max", "min"]))
+        .plug("uop", &Workload::new(&["-", "!"]))
+        .plug("v", &Workload::new(&["a", "b", "c"]))
+        .filter(Filter::Canon(vec![
+            "a".to_string(),
+            "b".to_string(),
+            "c".to_string(),
+        ]));
+    let new = run_workload(
+        nested_bops_arith,
+        all_rules.clone(),
+        Limits::rulefinding(),
+        true,
+    );
+    all_rules.extend(new);
+    println!("nested_bops_arith finished.");
+
+    let nested_bops_full = Workload::new(&["(bop e e)", "v"])
         .plug("e", &Workload::new(&["(bop v v)", "(uop v)", "v"]))
         .plug(
             "bop",
-            &Workload::new(&["&&", "||", "!=", "<=", "==", "max", "min"]),
+            &Workload::new(&["&&", "||", "!=", "<=", "==", "<", "max", "min"]),
         )
         .plug("uop", &Workload::new(&["-", "!"]))
         .plug("v", &Workload::new(&["a", "b", "c"]))
@@ -85,8 +112,6 @@ pub fn halide_rules() -> Ruleset<Pred> {
             "b".to_string(),
             "c".to_string(),
         ]));
-    println!("Writing nested-bops-full workload...");
-    nested_bops_full.to_file("nested-bops-full.terms");
     let new = run_workload(
         nested_bops_full,
         all_rules.clone(),
@@ -95,7 +120,8 @@ pub fn halide_rules() -> Ruleset<Pred> {
     );
     all_rules.extend(new.clone());
     println!("nested_bops_full finished.");
-    new.to_file("nested_bops_full.rules");
+    all_rules.to_file("thru_nested_bops_full.rules");
+
     all_rules.to_file("all-rules.rules");
     all_rules
 }
