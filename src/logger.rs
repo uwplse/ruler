53e2f63200800189ab14f17c51205640e8f247d7
vee-effekt <gannet@cs.washington.edu>
2023-04-12 10:02:58 -0700

    derivability

--- src/logger.rs
+++ src/logger.rs
@@ -16,75 +16,110 @@ pub fn write_output<L: SynthLanguage>(
     time_rules: Duration,
     derive: bool,
 ) {
-    // get information about the derivability of our ruleset vs. the baseline ruleset
-    let start = Instant::now();
-    let ((forwards_lhs, backwards_lhs), (lhs_f, lhs_b), results_lhs) =
-        get_derivability_results(ruleset, DeriveType::Lhs, baseline, derive);
-    let duration = start.elapsed();
-    println!(
-        "LHS: {} / {}, {} / {} in {} seconds.",
-        forwards_lhs,
-        baseline.clone().len(),
-        backwards_lhs,
-        ruleset.clone().len(),
-        duration.as_secs_f64()
-    );
-    let start = Instant::now();
-    let ((forwards_lhs_rhs, backwards_lhs_rhs), (lhs_rhs_f, lhs_rhs_b), results_lhs_rhs) =
-        get_derivability_results(ruleset, DeriveType::LhsAndRhs, baseline, derive);
-    let duration = start.elapsed();
-    println!(
-        "LHS/RHS: {} / {}, {} / {} in {} seconds.",
-        forwards_lhs_rhs,
-        baseline.clone().len(),
-        backwards_lhs_rhs,
-        ruleset.clone().len(),
-        duration.as_secs_f64()
-    );
-    let start = Instant::now();
-    let ((forwards_all, backwards_all), (all_f, all_b), results_all) =
-        get_derivability_results(ruleset, DeriveType::AllRules, baseline, derive);
-    let duration = start.elapsed();
-    println!(
-        "ALL: {} / {}, {} / {} in {} seconds.",
-        forwards_all,
-        baseline.clone().len(),
-        backwards_all,
-        ruleset.clone().len(),
-        duration.as_secs_f64()
-    );
+    if derive {
+        // get information about the derivability of our ruleset vs. the baseline ruleset
+        let start = Instant::now();
+        let ((forwards_lhs, backwards_lhs), (lhs_f, lhs_b), results_lhs) =
+            get_derivability_results(ruleset, DeriveType::Lhs, baseline);
+        let duration = start.elapsed();
+        println!(
+            "LHS: {} / {}, {} / {} in {} seconds.",
+            forwards_lhs,
+            baseline.clone().len(),
+            backwards_lhs,
+            ruleset.clone().len(),
+            duration.as_secs_f64()
+        );
+        let start = Instant::now();
+        let ((forwards_lhs_rhs, backwards_lhs_rhs), (lhs_rhs_f, lhs_rhs_b), results_lhs_rhs) =
+            get_derivability_results(ruleset, DeriveType::LhsAndRhs, baseline);
+        let duration = start.elapsed();
+        println!(
+            "LHS/RHS: {} / {}, {} / {} in {} seconds.",
+            forwards_lhs_rhs,
+            baseline.clone().len(),
+            backwards_lhs_rhs,
+            ruleset.clone().len(),
+            duration.as_secs_f64()
+        );
+        let start = Instant::now();
+        let ((forwards_all, backwards_all), (all_f, all_b), results_all) =
+            get_derivability_results(ruleset, DeriveType::AllRules, baseline);
+        let duration = start.elapsed();
+        println!(
+            "ALL: {} / {}, {} / {} in {} seconds.",
+            forwards_all,
+            baseline.clone().len(),
+            backwards_all,
+            ruleset.clone().len(),
+            duration.as_secs_f64()
+        );
 
-    // get linecount of recipe
-    let cnt = count_lines(recipe_name);
+        // get linecount of recipe
+        let cnt = count_lines(recipe_name);
 
-    let stats = json!({
-        "baseline_name": baseline_name,
-        "enumo_spec_name": recipe_name,
-        "loc": cnt,
-        "num_rules": ruleset.len(),
-        "rules": json!({"rules": ruleset.to_str_vec()}),
-        "num_baseline": baseline.len(),
-        "time": time_rules.as_secs_f64(),
-        "enumo_to_baseline_lhs": results_lhs,
-        "enumo_to_baseline_lhs_num": forwards_lhs,
-        "enumo_to_baseline_lhs_time": lhs_f.as_secs_f64(),
-        "enumo_to_baseline_lhs_rhs": results_lhs_rhs,
-        "enumo_to_baseline_lhsrhs_num": forwards_lhs_rhs,
-        "enumo_to_baseline_lhsrhs_time": lhs_rhs_f.as_secs_f64(),
-        "enumo_to_baseline_all": results_all,
-        "enumo_to_baseline_all_num": forwards_all,
-        "enumo_to_baseline_all_time": all_f.as_secs_f64(),
-        "baseline_to_enumo_lhs_num": backwards_lhs,
-        "baseline_to_enumo_lhs_time": lhs_b.as_secs_f64(),
-        "baseline_to_enumo_lhsrhs_num": backwards_lhs_rhs,
-        "baseline_to_enumo_lhsrhs_time": lhs_rhs_b.as_secs_f64(),
-        "baseline_to_enumo_all_num": backwards_all,
-        "baseline_to_enumo_all_time": all_b.as_secs_f64(),
-        "minimization strategy": "compress",
-    });
+        let stats = json!({
+            "baseline_name": baseline_name,
+            "enumo_spec_name": recipe_name,
+            "loc": cnt,
+            "num_rules": ruleset.len(),
+            "rules": json!({"rules": ruleset.to_str_vec()}),
+            "num_baseline": baseline.len(),
+            "time": time_rules.as_secs_f64(),
+            "enumo_to_baseline_lhs": results_lhs,
+            "enumo_to_baseline_lhs_num": forwards_lhs,
+            "enumo_to_baseline_lhs_time": lhs_f.as_secs_f64(),
+            "enumo_to_baseline_lhs_rhs": results_lhs_rhs,
+            "enumo_to_baseline_lhsrhs_num": forwards_lhs_rhs,
+            "enumo_to_baseline_lhsrhs_time": lhs_rhs_f.as_secs_f64(),
+            "enumo_to_baseline_all": results_all,
+            "enumo_to_baseline_all_num": forwards_all,
+            "enumo_to_baseline_all_time": all_f.as_secs_f64(),
+            "baseline_to_enumo_lhs_num": backwards_lhs,
+            "baseline_to_enumo_lhs_time": lhs_b.as_secs_f64(),
+            "baseline_to_enumo_lhsrhs_num": backwards_lhs_rhs,
+            "baseline_to_enumo_lhsrhs_time": lhs_rhs_b.as_secs_f64(),
+            "baseline_to_enumo_all_num": backwards_all,
+            "baseline_to_enumo_all_time": all_b.as_secs_f64(),
+            "minimization strategy": "compress",
+        });
 
-    std::fs::create_dir_all("nightly/data").unwrap_or_else(|e| panic!("Error creating dir: {}", e));
-    add_to_data_file("nightly/data/output.json".to_string(), stats);
+        std::fs::create_dir_all("nightly/data")
+            .unwrap_or_else(|e| panic!("Error creating dir: {}", e));
+        add_to_data_file("nightly/data/output.json".to_string(), stats);
+    } else {
+        let cnt = count_lines(recipe_name);
+
+        let stats = json!({
+            "baseline_name": baseline_name,
+            "enumo_spec_name": recipe_name,
+            "loc": cnt,
+            "num_rules": ruleset.len(),
+            "rules": json!({"rules": ruleset.to_str_vec()}),
+            "num_baseline": baseline.len(),
+            "time": time_rules.as_secs_f64(),
+            "enumo_to_baseline_lhs": vec![""],
+            "enumo_to_baseline_lhs_num": 0,
+            "enumo_to_baseline_lhs_time": Duration::default().as_secs_f64(),
+            "enumo_to_baseline_lhs_rhs": vec![""],
+            "enumo_to_baseline_lhsrhs_num": 0,
+            "enumo_to_baseline_lhsrhs_time": Duration::default().as_secs_f64(),
+            "enumo_to_baseline_all": vec![""],
+            "enumo_to_baseline_all_num": 0,
+            "enumo_to_baseline_all_time": Duration::default().as_secs_f64(),
+            "baseline_to_enumo_lhs_num": 0,
+            "baseline_to_enumo_lhs_time": Duration::default().as_secs_f64(),
+            "baseline_to_enumo_lhsrhs_num": 0,
+            "baseline_to_enumo_lhsrhs_time": Duration::default().as_secs_f64(),
+            "baseline_to_enumo_all_num": 0,
+            "baseline_to_enumo_all_time": Duration::default().as_secs_f64(),
+            "minimization strategy": "compress",
+        });
+
+        std::fs::create_dir_all("nightly/data")
+            .unwrap_or_else(|e| panic!("Error creating dir: {}", e));
+        add_to_data_file("nightly/data/output.json".to_string(), stats);
+    }
 }
 
 pub fn add_to_data_file(outfile: String, json: Value) {
@@ -143,47 +178,33 @@ pub fn get_derivability_results<L: SynthLanguage>(
     ruleset: &Ruleset<L>,
     derive_type: DeriveType,
     baseline: &Ruleset<L>,
-    derive: bool,
 ) -> ((usize, usize), (Duration, Duration), Value) {
-    if !derive {
-        (
-            (0, 0),
-            (Duration::default(), Duration::default()),
-            json!({
-                "enumo_derives_baseline_derivable": vec![""],
-                "enumo_derives_baseline_underivable": vec![""],
-                "baseline_derives_enumo_derivable": vec![""],
-                "baseline_derives_enumo_underivable": vec![""],
-            }),
-        )
+    let limits = if let DeriveType::AllRules = derive_type {
+        Limits {
+            iter: 2,
+            node: 100_000,
+        }
     } else {
-        let limits = if let DeriveType::AllRules = derive_type {
-            Limits {
-                iter: 2,
-                node: 100_000,
-            }
-        } else {
-            Limits::deriving()
-        };
+        Limits::deriving()
+    };
 
-        let start_f = Instant::now();
-        let (can_f, cannot_f) = ruleset.derive(derive_type, baseline, limits);
-        let time_f = start_f.elapsed();
-        let start_b = Instant::now();
-        let (can_b, cannot_b) = baseline.derive(derive_type, ruleset, limits);
-        let time_b = start_b.elapsed();
+    let start_f = Instant::now();
+    let (can_f, cannot_f) = ruleset.derive(derive_type, baseline, limits);
+    let time_f = start_f.elapsed();
+    let start_b = Instant::now();
+    let (can_b, cannot_b) = baseline.derive(derive_type, ruleset, limits);
+    let time_b = start_b.elapsed();
 
-        let derivability_results = json!({
-            "enumo_derives_baseline_derivable": &can_f.to_str_vec(),
-            "enumo_derives_baseline_underivable": &cannot_f.to_str_vec(),
-            "baseline_derives_enumo_derivable": &can_b.to_str_vec(),
-            "baseline_derives_enumo_underivable": &cannot_b.to_str_vec(),
-        });
+    let derivability_results = json!({
+        "enumo_derives_baseline_derivable": &can_f.to_str_vec(),
+        "enumo_derives_baseline_underivable": &cannot_f.to_str_vec(),
+        "baseline_derives_enumo_derivable": &can_b.to_str_vec(),
+        "baseline_derives_enumo_underivable": &cannot_b.to_str_vec(),
+    });
 
-        (
-            (can_f.len(), can_b.len()),
-            (time_f, time_b),
-            derivability_results,
-        )
-    }
+    (
+        (can_f.len(), can_b.len()),
+        (time_f, time_b),
+        derivability_results,
+    )
 }
