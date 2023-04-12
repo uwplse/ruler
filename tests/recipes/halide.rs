ef2be5dfc317a0be45c61290e7d28029fc349d32
vee-effekt <gannet@cs.washington.edu>
2023-04-12 10:14:40 -0700

    fancy

--- tests/recipes/halide.rs
+++ tests/recipes/halide.rs
@@ -98,5 +98,40 @@ pub fn halide_rules() -> Ruleset<Pred> {
         true,
     );
     all_rules.extend(new.clone());
+    let select_max = Workload::new(&["(max s s)", "(min s s)", "(select v s s)"])
+        .plug("s", &Workload::new(&["(select v v v)", "(bop v v)", "v"]))
+        .plug("v", &Workload::new(&["a", "b", "c", "d", "e", "f"]))
+        .plug("bop", &Workload::new(&["+", "-", "*", "/", "min", "max"]))
+        .filter(Filter::Canon(vec![
+            "a".to_string(),
+            "b".to_string(),
+            "c".to_string(),
+            "d".to_string(),
+            "e".to_string(),
+            "f".to_string(),
+        ]));
+    let new = run_workload(select_max, all_rules.clone(), Limits::rulefinding(), true);
+    println!("select_max finished.");
+    new.to_file("select-max.rules");
+    all_rules.extend(new.clone());
+    let select_arith = Workload::new(&["(select v e e)", "(bop v e)", "(bop e v)"])
+        .plug("e", &Workload::new(&["(bop v v)", "(select v v v)", "v"]))
+        .plug(
+            "bop",
+            &Workload::new(&["+", "-", "*", "<", "/"]),
+        )
+        .plug("v", &Workload::new(&["a", "b", "c", "d", "e", "f"]))
+        .filter(Filter::Canon(vec![
+            "a".to_string(),
+            "b".to_string(),
+            "c".to_string(),
+            "d".to_string(),
+            "e".to_string(),
+            "f".to_string(),
+        ]));
+    let new = run_workload(select_arith, all_rules.clone(), Limits::rulefinding(), true);
+    println!("select_arith finished.");
+    new.to_file("select-arith.rules");
+    all_rules.extend(new.clone());
     all_rules
 }
