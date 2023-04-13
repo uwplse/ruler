a6b4357c11a28f68e9def0735d311aa9bbeb914e
Anjali Pal <anjalip@cs.washington.edu>
2023-04-13 14:48:40 -0700

    no more latex

--- infra/nightly-resources/generate.js
+++ infra/nightly-resources/generate.js
@@ -233,36 +233,38 @@ function onGenerateClick(version) {
 }
 
 function generateLatex(version, tableData, ignoreColumns) {
-  let columnNames = Object.keys(tableData[0]);
-  let lines = [
-    String.raw`\begin{table}`,
-    String.raw`\resizebox{\textwidth}{!}{%`,
-    String.raw`\begin{tabular}{` +
-      "l".repeat(columnNames.length - ignoreColumns.length) +
-      "}",
-  ];
+  document.getElementById("latex").innerHTML =
+    "This feature is not currently supported.";
+  // let columnNames = Object.keys(tableData[0]);
+  // let lines = [
+  //   String.raw`\begin{table}`,
+  //   String.raw`\resizebox{\textwidth}{!}{%`,
+  //   String.raw`\begin{tabular}{` +
+  //     "l".repeat(columnNames.length - ignoreColumns.length) +
+  //     "}",
+  // ];
 
-  lines.push(getFormattedHeader(version, columnNames));
+  // lines.push(getFormattedHeader(version, columnNames));
 
-  tableData.forEach((row) => {
-    let line =
-      Object.keys(row)
-        .filter((key) => !ignoreColumns.includes(key))
-        .map((key) => row[key])
-        .join(" & ") + " \\\\";
-    lines.push(line.replaceAll("%", "\\%"));
-  });
+  // tableData.forEach((row) => {
+  //   let line =
+  //     Object.keys(row)
+  //       .filter((key) => !ignoreColumns.includes(key))
+  //       .map((key) => row[key])
+  //       .join(" & ") + " \\\\";
+  //   lines.push(line.replaceAll("%", "\\%"));
+  // });
 
-  lines.push(String.raw`\end{tabular}%`);
-  lines.push(String.raw`}`);
-  lines = lines.concat(getCaption(version));
-  lines.push(String.raw`\label{table:${version}}`);
-  lines.push(String.raw`\end{table}`);
+  // lines.push(String.raw`\end{tabular}%`);
+  // lines.push(String.raw`}`);
+  // lines = lines.concat(getCaption(version));
+  // lines.push(String.raw`\label{table:${version}}`);
+  // lines.push(String.raw`\end{table}`);
 
-  let s = lines.join("\n");
-  let elem = document.getElementById("latex");
-  elem.innerHTML = s;
-  elem.style.height = "200px";
+  // let s = lines.join("\n");
+  // let elem = document.getElementById("latex");
+  // elem.innerHTML = s;
+  // elem.style.height = "200px";
 }
 
 function consolidateColumns(dataObj, consolidatedName, columnNames) {
