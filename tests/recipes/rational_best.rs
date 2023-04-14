479c7dbf483a0e12bf1d91182f1ffa5bb5ae3b1b
oflatt <oflatt@gmail.com>
2023-04-14 12:46:05 -0700

    filter out crazy rules from factor

--- tests/recipes/rational_best.rs
+++ tests/recipes/rational_best.rs
@@ -85,12 +85,13 @@ pub fn best_enumo_recipe() -> Ruleset<Math> {
     let factor_div = Workload::new(["(/ v v)"]).plug("v", &factor_term);
 
     let factor_rules = Math::run_workload_conditional(factor_div, rules.clone(), limits, false);
+    // don't include rules that already contain division
     let factor_rules_filtered = Ruleset(
         factor_rules
             .0
             .into_iter()
             .filter_map(|rule| {
-                if rule.1.lhs.to_string() == "(- ?a ?a)" {
+                if rule.1.lhs.to_string().contains("/") {
                     None
                 } else {
                     Some(rule)
