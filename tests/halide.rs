a96f1413ef158d03fe52fa247a0e593abc5a1e05
Anjali Pal <anjalip@cs.washington.edu>
2025-05-13 13:31:11 -0700

    Update halide.rs

--- tests/halide.rs
+++ tests/halide.rs
@@ -298,37 +298,37 @@ mod test {
         Limits,
     };
 
-    // #[test]
-    // fn run() {
-    //     // Skip this test in github actions
-    //     if std::env::var("CI").is_ok() && std::env::var("SKIP_RECIPES").is_ok() {
-    //         return;
-    //     }
+    #[test]
+    fn run() {
+        // Skip this test in github actions
+        if std::env::var("CI").is_ok() && std::env::var("SKIP_RECIPES").is_ok() {
+            return;
+        }
 
-    //     let start = Instant::now();
-    //     let all_rules = halide_rules();
-    //     let duration = start.elapsed();
+        let start = Instant::now();
+        let all_rules = halide_rules();
+        let duration = start.elapsed();
 
-    //     // oopsla-halide-baseline branch
-    //     // Run on nightly 4/12/2023
-    //     // time cargo run --release --bin halide -- synth --iters 1 --use-smt
-    //     // real	0m2.707s
-    //     // user	0m2.681s
-    //     // sys	0m0.028s
-    //     let baseline: Ruleset<Pred> = Ruleset::from_file("baseline/halide.rules");
-    //     let oopsla_halide: Ruleset<Pred> = Ruleset::from_file("baseline/oopsla-halide.rules");
-    //     let oopsla_duration = Duration::from_secs_f32(3.354);
+        // oopsla-halide-baseline branch
+        // Run on nightly 4/12/2023
+        // time cargo run --release --bin halide -- synth --iters 1 --use-smt
+        // real	0m2.707s
+        // user	0m2.681s
+        // sys	0m0.028s
+        let baseline: Ruleset<Pred> = Ruleset::from_file("baseline/halide.rules");
+        let oopsla_halide: Ruleset<Pred> = Ruleset::from_file("baseline/oopsla-halide.rules");
+        let oopsla_duration = Duration::from_secs_f32(3.354);
 
-    //     logger::write_baseline(&all_rules, "halide", &baseline, "halide", duration);
+        logger::write_baseline(&all_rules, "halide", &baseline, "halide", duration);
 
-    //     logger::write_baseline(
-    //         &oopsla_halide,
-    //         "oopsla halide (1 iter)",
-    //         &baseline,
-    //         "halide",
-    //         oopsla_duration,
-    //     );
-    // }
+        logger::write_baseline(
+            &oopsla_halide,
+            "oopsla halide (1 iter)",
+            &baseline,
+            "halide",
+            oopsla_duration,
+        );
+    }
 
     #[test]
     fn enumo_exhaustive() {
